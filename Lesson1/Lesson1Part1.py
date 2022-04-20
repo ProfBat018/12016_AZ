4a64f9dd4c15bfa7d1bf43b608df8e94e6a4cec8
elvin <elvin.azim01@gmail.com>
2022-04-20 19:54:38 +0400

    IF statement

---
+++ Lesson1/Lesson1Part1.py
@@ -0,0 +1,75 @@
+# <editor-fold desc="Quotes">
+
+# print("'Hello World'")
+# print('"Hello World"')
+
+# </editor-fold>
+
+# <editor-fold desc="Escape sequences">
+
+# print("\"Hello World\"")
+# print('\'Hello World\'')
+# print("Hello\nWorld")
+# print("Hello\tWorld")
+# print("Hello\\World")
+
+# </editor-fold>
+
+# <editor-fold desc="End & Sep">
+
+# <editor-fold desc="END">
+
+# print("Hello", end=" ")
+# print("World")
+
+# </editor-fold>
+
+# <editor-fold desc="SEP">
+
+# print(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, sep=", ", end='-')
+# print("Hello")
+
+# </editor-fold>
+
+
+# </editor-fold>
+
+# <editor-fold desc="Variables">
+#
+# first_number = 5
+# second_number = 6
+#
+# result = first_number + second_number
+#
+# print(first_number, '+', second_number, '=', result)
+#
+# print("{} + {} = {}".format(first_number, second_number, result))     # string formatting
+#
+# print(f"{first_number} + {second_number} = {result}")
+
+# </editor-fold>
+
+# <editor-fold desc="Input">
+#
+#
+# first_num = int(input("Enter first number: "))     # 4
+# second_num = int(input("Enter second number: "))    # 4
+#
+# result = first_num + second_num
+#
+# print(f"{first_num} + {second_num} = {result}")
+
+# </editor-fold>
+
+# <editor-fold desc="Operators">
+
+# first_num = 2
+# second_num = 3
+#
+# res1 = first_num % second_num
+# res2 = first_num // second_num
+# res3 = first_num ** second_num
+#
+# print(res1, res2, res3)
+
+# </editor-fold>
---
+++ Lesson2/main.py
@@ -0,0 +1,54 @@
+# <editor-fold desc="Part1">
+
+# a = 5
+# b = 6
+
+# print(a > b)
+# print(a < b)
+# print(a >= b)
+# print(a <= b)
+# print(a == b)
+# print(a != b)
+
+# a += b  # a = a + b
+# </editor-fold>
+
+# <editor-fold desc="Type Casting">
+
+# Explicit type casting
+# Implicit type casting
+
+# num1 = bool(input("Enter first number: "))      # Explicit type casting
+# num2 = bool(input("Enter second number: "))     # 2
+#
+# print(num1 + num2)  # Implicit bool to int
+
+
+# print(str(bool(2)) + str(bool('a')))
+# print(bool(2) + bool('a'))
+
+# </editor-fold>
+
+num1 = int(input())     # 6
+num2 = int(input())     # 3
+
+
+# if num1 == num2:
+#     print(num1)
+# elif num1 > num2:
+#     print("num1 > num2")
+# elif num1 < num2:
+#     print("num1 < num2")
+# else:
+#     print("Error")
+
+# if num1 == num2:
+#     print(num1)
+# else:
+#     if num1 > num2:
+#         print("num1 > num2")
+#     else:
+#         if num1 < num2:
+#             print("num1 < num2")
+#         else:
+#             print("Error")
