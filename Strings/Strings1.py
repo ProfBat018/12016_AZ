579b20cb0b681cdecd3e6934c6618cdd7bb6609f
e.azimov <elvin.azim01@gmail.com>
2022-05-04 21:14:32 +0400

    String lesson 1

---
+++ Strings/Strings1.py
@@ -0,0 +1,60 @@
+# name = "Elvin"
+
+import os
+
+# <editor-fold desc="String indexes">
+# print(name)
+# print(name[0])
+# print(name[-1])
+
+# </editor-fold>
+
+# <editor-fold desc="String slices">
+
+# print(name[2:4])
+# print(name[:3])
+# print(name[1:])
+# print(name[::2])
+# print(name[::])
+# print(name[::-1])
+# </editor-fold>
+
+# <editor-fold desc="String methods">
+
+# <editor-fold desc="Boolean methods">
+# text = chr(0)   # \0
+# print(text.isalpha())   # is alphabet
+# print(text.isalnum())     # is alpha-numeric
+# print(text.isnumeric())
+# print(text.isdigit())
+# print(text.isdecimal())
+# print(text.isupper())
+# print(text.islower())
+# print(text.istitle())
+# print(text.isspace())
+# print(text.isprintable())
+
+# </editor-fold>
+
+text = "something in the way"
+
+# print(text.index("w"))
+# print(text.count('i'))
+
+# text = text.title()
+# text = text.capitalize()
+# text += "a"
+
+# new_text = str()
+#
+# for i in range(len(text)):
+#     if text[i] == 'i':
+#         new_text += 'o'
+#     else:
+#         new_text += text[i]
+#
+# print(new_text)
+
+
+# </editor-fold>
+
