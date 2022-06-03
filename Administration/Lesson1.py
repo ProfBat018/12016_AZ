80bfa0926d7d6cf5e1cfa937e9e51b077a74ccae
e.azimov <elvin.azim01@gmail.com>
2022-06-03 20:39:59 +0400

    speedtest

---
+++ Administration/Lesson1.py
@@ -0,0 +1,15 @@
+# import socket
+# import psutil
+# from windows_tools import product_key
+# from windows_tools import updates
+# from windows_tools import server
+# from pprint import pprint
+#
+# print(product_key.get_windows_product_key_from_reg())
+# pprint(updates.get_windows_updates())
+#
+
+
+# from windows_tools import product_key
+# print(product_key.get_windows_product_key_from_reg())
+#
